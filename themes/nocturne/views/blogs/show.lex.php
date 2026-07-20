13a5156f8abda3531482e08b920af5aefa714539
webdevartisan <webdevartisan@mail.ru>
2026-07-20 17:33:03 +0300

    Update Nocturne theme views for engagement features

--- themes/nocturne/views/blogs/show.lex.php
+++ themes/nocturne/views/blogs/show.lex.php
@@ -74,8 +74,8 @@ $blogTitle = e($blog['blog_name'] ?? 'Nocturne');
     $fTitle = e($featuredPost['title'] ?? 'Untitled');
     $fExc = e($featuredPost['excerpt'] ?? '');
     $fAuthor = profile_link(
-      $featuredPost['author_name'] ?? ($user['display_name_cached'] ?? $user['username'] ?? ''),
-      $user['public_profile_slug'] ?? null
+        $featuredPost['author_name'] ?? ($user['display_name_cached'] ?? $user['username'] ?? ''),
+        $user['public_profile_slug'] ?? null
     );
     $fDate = e($featuredPost['published_at'] ?? '');
     $fCat = e($featuredPost['category'] ?? 'Article');
